6f998a2287557db3948d408f15b9ecd8a74e01dc
Carlos de Paula <me@carlosedp.com>
2023-04-25 16:31:28 -0300

    Add helper test.

--- zio-channel/test/src/HelpersSpec.scala
+++ zio-channel/test/src/HelpersSpec.scala
@@ -15,6 +15,18 @@ object HelpersSpec extends ZIOSpecDefault:
                      for
                        r <- ZIO.succeed(false)
                      yield r
-          yield assertCompletes,
+          yield assertCompletes
+        ,
+        test("foreverWhile loop should finish after 10 loops"):
+          var counter: Int = 0
+          for
+            res <- foreverWhile:
+                     for
+                       r <- ZIO.succeed {
+                              counter += 1
+                              counter < 10
+                            }
+                     yield r
+          yield assert(counter)(equalTo(10)),
       )
     )
